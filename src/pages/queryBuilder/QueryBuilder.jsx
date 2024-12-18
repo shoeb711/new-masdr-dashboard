a960c7144917e52bfd3f5984a71264ac2dffe43f
deep-colla-786 <newmoiz356@gmail.com>
2024-12-18 14:19:42 +0530

    integrating tennat dropdown api

--- src/pages/queryBuilder/QueryBuilder.jsx
+++ src/pages/queryBuilder/QueryBuilder.jsx
@@ -1,4 +1,3 @@
-
 import { Editor, loader } from "@monaco-editor/react";
 import QueryBuilderTab from "components/queryBuilderTab/QueryBuilderTab";
 import SettingDrawer from "components/settingDrawer/SettingDrawer";
@@ -53,8 +52,6 @@ const QueryBuilder = () => {
     editorRef.current = editor;
     editor.focus();
   };
-
-
 
   const fetchChartData = async () => {
     if (!queryValue) return;
@@ -145,93 +142,94 @@ const QueryBuilder = () => {
   };
 
   return (
-    <>
-      <div>
-        <div className="flex justify-between items-center p-4">
-          <h1 className="text-lg font-bold">QUERY BUILDER</h1>
-          {role === userRole.SUPER_ADMIN && (
-            <Dropdown buttonText={selectedTenant || "Select Tenant"} items={tenants} />
-          )}
-        </div>
+    <div className="h-[calc(100%-64px)]">
+      <div className="flex justify-between items-center p-4">
+        <h1 className="text-lg font-bold">QUERY BUILDER</h1>
+        {role === userRole.SUPER_ADMIN && (
+          <Dropdown
+            buttonText={selectedTenant || "Select Tenant"}
+            items={tenants}
+          />
+        )}
+      </div>
 
-        <div className="flex flex-col justify-between h-full">
-          <div>
-            <div className="bg-gray-100">
-              <div className="flex justify-between items-center border-b border-gray-300 px-3 py-2">
-                <div className="w-96">
-                  <InputField placeholder="Enter Chart Title" />
-                </div>
+      <div className="flex flex-col justify-between h-full">
+        <div>
+          <div className="bg-gray-100">
+            <div className="flex justify-between items-center border-b border-gray-300 px-3 py-2">
+              <div className="w-96">
+                <InputField placeholder="Enter Chart Title" />
               </div>
-              <section className="flex items-start">
-                <div className="w-[97%]">
-                  <Editor
-                    options={options}
-                    height="45vh"
-                    width="100%"
-                    theme="myTheme"
-                    className="bg-gray-100"
-                    defaultLanguage="sql"
-                    onMount={onMount}
-                    value={queryValue}
-                    onChange={(val) => {
-                      setQueryValue(val);
-                    }}
-                  />
-                </div>
-                <div className="flex flex-col gap-5 py-2 items-center pe-4">
-                  {editorEvents.map((item, index) => (
-                    <div key={index}>
-                      <button
-                        onClick={() => {
-                          if (item.name === "Run Query") {
-                            fetchChartData();
-                          } else if (editorRef.current && item.action) {
-                            item.action(editorRef.current);
-                          }
-                        }}
-                      >
-                        <item.icon
-                          title={item.name}
-                          aria-hidden="true"
-                          className="size-6 shrink-0 cursor-pointer"
-                        />
-                      </button>
-                    </div>
-                  ))}
-                </div>
-              </section>
             </div>
-
-            <div>{renderQueryOutput()}</div>
+            <section className="flex items-start">
+              <div className="w-[97%]">
+                <Editor
+                  options={options}
+                  height="45vh"
+                  width="100%"
+                  theme="myTheme"
+                  className="bg-gray-100"
+                  defaultLanguage="sql"
+                  onMount={onMount}
+                  value={queryValue}
+                  onChange={(val) => {
+                    setQueryValue(val);
+                  }}
+                />
+              </div>
+              <div className="flex flex-col gap-5 py-2 items-center pe-4">
+                {editorEvents.map((item, index) => (
+                  <div key={index}>
+                    <button
+                      onClick={() => {
+                        if (item.name === "Run Query") {
+                          fetchChartData();
+                        } else if (editorRef.current && item.action) {
+                          item.action(editorRef.current);
+                        }
+                      }}
+                    >
+                      <item.icon
+                        title={item.name}
+                        aria-hidden="true"
+                        className="size-6 shrink-0 cursor-pointer"
+                      />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </section>
           </div>
 
-          <QueryBuilderTab
-            setQueryBuilderTab={setQueryBuilderTab}
-            queryBuilderTab={queryBuilderTab}
-          />
+          <div>{renderQueryOutput()}</div>
         </div>
 
-        <CustomFlyoutModal
-          isOpen={queryBuilderTab === queryBuilderTabEnum.VISUALIZATION}
-          onClose={() => setQueryBuilderTab("")}
-          modalClassName="overflow-visible"
-        >
-          <VisualizationDrawer
-            setSelectedChartType={setSelectedChartType}
-            selectedChartType={selectedChartType}
-            onClose={() => setQueryBuilderTab("")}
-          />
-        </CustomFlyoutModal>
-
-        <CustomFlyoutModal
-          isOpen={queryBuilderTab === queryBuilderTabEnum.SETTING}
-          onClose={() => setQueryBuilderTab("")}
-        >
-            onClose={() => setQueryBuilderTab("")}
-            <SettingDrawer  />
-        </CustomFlyoutModal>
+        <QueryBuilderTab
+          setQueryBuilderTab={setQueryBuilderTab}
+          queryBuilderTab={queryBuilderTab}
+        />
       </div>
-    </>
+
+      <CustomFlyoutModal
+        isOpen={queryBuilderTab === queryBuilderTabEnum.VISUALIZATION}
+        onClose={() => setQueryBuilderTab("")}
+        modalClassName="overflow-visible"
+      >
+        <VisualizationDrawer
+          setSelectedChartType={setSelectedChartType}
+          selectedChartType={selectedChartType}
+          onClose={() => setQueryBuilderTab("")}
+        />
+      </CustomFlyoutModal>
+
+      <CustomFlyoutModal
+        isOpen={queryBuilderTab === queryBuilderTabEnum.SETTING}
+        onClose={() => setQueryBuilderTab("")}
+      >
+        onClose={() => setQueryBuilderTab("")}
+        <SettingDrawer />
+      </CustomFlyoutModal>
+    </div>
   );
 };
 
