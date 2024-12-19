6a7a6d8b050840d4a3523a15f507f62837070034
Shoeb Shaikh <shoeb.shaikh@vervali.com>
2024-12-19 13:46:05 +0530

    Fix: UI fixes

--- src/components/settingDrawer/SettingDrawer.jsx
+++ src/components/settingDrawer/SettingDrawer.jsx
@@ -41,7 +41,7 @@ const SettingDrawer = ({ onClose }) => {
                     selectedSettingTab === tab.name
                       ? "border-indigo-500 text-indigo-600"
                       : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                    "w-1/4 border-b-2 px-1 py-4 text-center text-sm font-medium capitalize"
+                    "w-1/4 border-b-2 px-1 pb-4 text-center text-sm font-medium capitalize"
                   )}
                 >
                   {tab.name}
