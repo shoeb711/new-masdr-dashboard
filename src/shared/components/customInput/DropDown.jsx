17ab194224972f60bb3cd5c9fadc61c934b5339c
Shoeb Shaikh <shoeb.shaikh@vervali.com>
2025-01-02 11:54:07 +0530

    fix: change in add query builder and env changes

--- src/shared/components/customInput/DropDown.jsx
+++ src/shared/components/customInput/DropDown.jsx
@@ -42,7 +42,7 @@ const Dropdown = ({ buttonText = "Options", items = [] }) => {
 
       {isOpen && !!items?.length && (
         <div
-          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+          className="absolute z-20 right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
           role="menu"
           aria-orientation="vertical"
         >
